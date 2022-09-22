9ca437b4997c19ae37fe9395d41eeaf822f289d3
Ron Sharabi <ronsh0111@gmail.com>
2022-09-22 16:41:49 +0300

    added docker-compose.yml & dump.sql

---
+++ database/dump.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `mydb`;
+
+USE `mydb`
+
+CREATE TABLE IF NOT EXISTS `rooms` (
+    `username` varchar(255) NOT NULL,
+    `msg` varchar(1000) NOT NULL,
+    `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+)
